d403de6b385137cf4bb2b8adaaec5d5935aae8ee
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-06-06 19:59:30 +0330

    gallery for desctop complete

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,7 +1,26 @@
 import { useEffect, useState } from "react";
 import sample from "../../api/nail-sample";
-import { Dialog } from "@mui/material";
+import { Dialog, Slide } from "@mui/material";
 import { X, ChevronLeft, ChevronRight } from "lucide-react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+};
 
 export default function Gallery() {
   return (
@@ -13,15 +32,23 @@ export default function Gallery() {
       </div>
       <div className="bg-[#f5eae4] mt-10">
         <div className="container-primary p-9">
-          <div className="flex flex-col justify-center items-center">
-            <div className="grid grid-cols-4 space-x-4 w-max px-4 space-y-4">
-              {sample.map((item) => (
-                <div key={item.id} className="min-w-[250px] max-w-[250px] bg-white rounded-xl shadow-md cursor-pointer hover:scale-105 transition duration-300">
-                  <img className="rounded-xl w-full h-48 object-cover" src={item.image_url} alt={item.title} />
-                </div>
-              ))}
+          <div className="">
+            <div className="max-w-6xl mx-auto px-4 mt-10">
+              <Slider {...settings}>
+                {sample.map((product) => (
+                  <div key={product.id} className="p-2 min-w-[250px] max-w-[250px] bg-white rounded-xl shadow-md cursor-pointer hover:scale-105 transition duration-300">
+                    <div className="bg-white shadow rounded-lg p-4 text-center">
+                      <img
+                        src={product.image_url}
+                        alt={product.title}
+                        className=" rounded-xl w-full h-48 object-cover"
+                      />
+                    </div>
+                  </div>
+                ))}
+              </Slider>
             </div>
-            <div>
+            <div className="text-center p-3">
               <button className="cursor-pointer hover:text-white duration-200 bg-[#bf998e] p-2 rounded-lg font-bold text-center">
                 رزرو آنلاین
               </button>
