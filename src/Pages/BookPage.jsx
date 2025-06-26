ff311b7adeab5b9df0ea9419046609785f8c24ad
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-06-26 19:46:32 +0330

    fix bog

--- src/Pages/BookPage.jsx
+++ src/Pages/BookPage.jsx
@@ -1,9 +1,11 @@
 import Book from "../components/Book";
 
-export default function BookPage(){
-    return(
-        <>
+export default function BookPage() {
+  return (
+    <>
+      <div className="container-primary h-screen">
         <Book />
-        </>
-    )
+      </div>
+    </>
+  );
 }
