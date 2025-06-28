50aa56b135173b8ca5d83a9020d136401990b155
Milad-F84 <mfarajollahifrontend@gmail.com>
2025-06-28 19:12:14 +0330

    contact page complete

--- src/Pages/ContactUsPage.jsx
+++ src/Pages/ContactUsPage.jsx
@@ -1,9 +1,13 @@
 import ContactUs from "../components/ContactUs";
+import Location from "../components/Location";
 
-export default function ContactUsPage(){
-    return(
-        <>
+export default function ContactUsPage() {
+  return (
+    <>
+      <Location />
+      <div className="bg-[#f5eae4]">
         <ContactUs />
-        </>
-    )
+      </div>
+    </>
+  );
 }
--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -7,18 +7,25 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import EmailIcon from "@mui/icons-material/Email";
 import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
 
 const schema = yup.object({
   firstName: yup.string().required("نام الزامی است."),
-  lastName: yup.string().required("نام خانوادگی کاربری الزامی است."),
+  lastName: yup.string().required("نام خانوادگی الزامی است."),
   phoneNum: yup
     .string()
     .required("شماره تلفن الزامی است.")
     .min(11, "شماره تلفن باید حداقل 11 کاراکتر باشد."),
+  message: yup.string().required("پیام الزامی است."),
 });
 
 export default function ContactUs() {
   const navigate = useNavigate();
+
+  const [messagee, setMessage] = useState("");
 
   const {
     register,
@@ -29,54 +36,63 @@ export default function ContactUs() {
   });
 
   const onSubmit = () => {
-    login();
-    navigate("/Booking");
+    const subject = "ارتباط با مدیر";
+    const message = `${messagee}`;
+    const email = "miladfarajollahi84@gmail.com";
+    const url = `mailto:${email}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(message)}`;
+    window.open(url, "_blank");
   };
 
   return (
     <>
-      <Location />
-
-      <div className="bg-[#f5eae4] mt-5">
-        <div>
-          <div>
+      <div className="bg-[#f5eae4] mt-5 flex flex-col md:flex-row md:justify-between md:items-center container-primary">
+        <div className="md:w-[50%] p-3 flex flex-col gap-4 justify-center">
+          <div className="flex items-center gap-2">
             <PhoneIcon />
-            <p>شماره های تماس</p>
+            <p>شماره های تماس:</p>
             <span>09335407792</span>
           </div>
-          <div>
+          <div className="flex items-center gap-2">
             <PriorityHighIcon />
             <p>
               توجه داشته باشید که تنها ایمیل های دریافتی از ایمیل بالا از سمت
               مجموعه غزاله فرج الهی بوده و معتبر خواهد بود.
             </p>
           </div>
-          <div>
+          <div className="flex items-center gap-2">
             <PlaceIcon />
             <p>
               لورم ایپسوم متن ساختگی با تولید سادگی از صنعت چاپ، و با استفاده از
               طراحان گرافیک است
             </p>
           </div>
-          <div>
-            <div>
+          <div className="flex flex-col gap-2 justify-center">
+            <div className="flex items-center gap-2">
               <PersonIcon />
               <p>شبکه های اجتماعی</p>
             </div>
-            <div>
+            <a
+              href="https://www.instagram.com/ghazaleh.farajollahi/"
+              className="flex items-center gap-2"
+            >
               <InstagramIcon />
               <p>اینستاگرام</p>
-            </div>
-            <div>
+            </a>
+            <a
+              href="https://wa.me/989335407792"
+              className="flex items-center gap-2"
+            >
               <WhatsAppIcon />
               <p>واتساپ</p>
-            </div>
-            <div>
+            </a>
+            <a href="sms:09335407792" className="flex items-center gap-2">
               <EmailIcon />
               <p>پیامک</p>
-            </div>
+            </a>
           </div>
-          <div>
+          <div className="flex items-center gap-2">
             <PriorityHighIcon />
             <p>
               همچنین برای ارتباط مستقیم با مدیریت مجموعه از طریق فرم زیر اقدام
@@ -84,33 +100,50 @@ export default function ContactUs() {
             </p>
           </div>
         </div>
-        <div>
+        <div className="md:w-[50%] p-3">
           <form
             onSubmit={handleSubmit(onSubmit)}
             className="max-w-md mx-auto mt-10"
           >
             <div className="flex flex-col gap-2 justify-center">
-              <label>نام کاربری:</label>
+              <label>نام:</label>
               <input
-                {...register("username")}
+                {...register("firstName")}
                 className="border w-full p-2 mb-2 rounded"
               />
-              <p className="text-red-600">{errors.username?.message}</p>
+              <p className="text-red-600">{errors.firstName?.message}</p>
             </div>
             <div className="flex flex-col gap-2 justify-center">
-              <label>رمز ورود:</label>
+              <label>نام خانوادگی:</label>
               <input
-                type="password"
-                {...register("password")}
+                {...register("lastName")}
                 className="border w-full p-2 mb-2 rounded"
               />
-              <p className="text-red-600">{errors.password?.message}</p>
+              <p className="text-red-600">{errors.lastName?.message}</p>
+            </div>
+            <div className="flex flex-col gap-2 justify-center">
+              <label>تلفن همراه:</label>
+              <input
+                type="tel"
+                {...register("phoneNum")}
+                className="border w-full p-2 mb-2 rounded"
+              />
+              <p className="text-red-600">{errors.phoneNum?.message}</p>
+            </div>
+            <div className="flex flex-col gap-2 justify-center">
+              <label>پیام:</label>
+              <input
+              onChange={(item) => setMessage(item.target.value)}
+                {...register("message")}
+                className="border w-full p-2 mb-2 rounded"
+              />
+              <p className="text-red-600">{errors.message?.message}</p>
             </div>
             <button
               type="submit"
               className="bg-blue-500 text-white py-2 px-4 rounded w-full cursor-pointer"
             >
-              ورود
+              ارسال
             </button>
           </form>
         </div>
